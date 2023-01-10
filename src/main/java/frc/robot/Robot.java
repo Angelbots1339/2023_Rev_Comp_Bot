45fb03e20d5457e64e3c1cc6c377dc7dfc555156
CooperSurma <896779@dpsk12.net>
2023-01-09 16:19:48 -0800

    fix joystick name

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,7 +21,7 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
-  XboxController joystick = new XboxController(0);
+  XboxController Joystick = new XboxController(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
