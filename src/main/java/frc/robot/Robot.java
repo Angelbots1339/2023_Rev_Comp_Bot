1a4b9f7a0d064d6b8c928b2211edb60858aec8c6
CooperSurma <896779@dpsk12.net>
2023-01-17 15:23:49 -0800

    remove bad controller implementation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import edu.wpi.first.wpilibj.XboxController;
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -20,8 +18,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
-  XboxController Joystick = new XboxController(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
